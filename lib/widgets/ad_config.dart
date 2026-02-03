7cce38d417b40098a1b952f812a35e6c34844ee5
Vrushabh <vrushabhbhanarkar3@gmail.com>
2026-02-04 00:15:31 +0530

    Implemented the ad ids

--- lib/widgets/ad_config.dart
+++ lib/widgets/ad_config.dart
@@ -4,22 +4,16 @@ class AdConfig {
   static bool enableAds = true;
 
   // Interstitial Ad Unit ID
-  // static String interstitialAdUnitId =
-  //     'ca-app-pub-6586073221878389/3484062745'; // Real production ID
   static String interstitialAdUnitId =
-      'ca-app-pub-3940256099942544/1033173712'; // TEST ad unit
+      'ca-app-pub-6586073221878389/3484062745'; // Production ad unit
 
   // Banner Ad Unit ID
-  // static String bannerAdUnitId =
-  //     'ca-app-pub-6586073221878389/7339693065'; // Real production ID
   static String bannerAdUnitId =
-      'ca-app-pub-3940256099942544/6300978111'; // TEST ad unit
+      'ca-app-pub-6586073221878389/7339693065'; // Production ad unit
 
   static bool enableNativeAds = true; // Set to true to enable native ads
-  // static const String nativeAdUnitId =
-  //     'ca-app-pub-6586073221878389/1324517855'; // Real production native ad unit
   static const String nativeAdUnitId =
-      'ca-app-pub-3940256099942544/2247696110'; // TEST native ad unit
+      'ca-app-pub-6586073221878389/1324517855'; // Production native ad unit
 
   // Optionally, add more ad types/configs here
 }
